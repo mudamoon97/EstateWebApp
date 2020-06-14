0f6e16f35a626a7f5459780332e4da9adc4acd53
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-06-14 01:05:50 +0100

    Added Entities

---
+++ src/EstateApp.Data/DatbaseContexts/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHouseApp.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        public string  Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
